79742ed235fdaf8f8d9a66233e9b669d89462f61
Creysys <essub_naj@yahoo.de>
2015-04-04 21:19:08 +0200

    Setup

---
+++ src/main/java/com/eg/SpectralProjection/SpectralProjection.java
@@ -0,0 +1,21 @@
+package com.eg.SpectralProjection;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SpectralProjection.MODID, name = SpectralProjection.MODNAME,version = SpectralProjection.VERSION)
+public class SpectralProjection
+{
+    public static final String MODID = "SpectralProjection";
+    public static final String modid = "spectralprojection";
+    public static final String MODNAME = "Spectral Projection";
+    public static final String VERSION = "1.0";
+
+    
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
